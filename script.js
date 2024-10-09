9511f80899aaa6643b4931b63ac9b61aea108017
Garrett Lamothe <garrettlamothe@garretts-mbp.myfiosgateway.com>
2024-10-08 20:24:44 -0400

    test

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
+axios.get("https://dummyjson.com/recipes")
+    .then(function(response){
+        console.log(response)
+    })
 
-let score=prompt("score")
-if (score > 100) {
-    prompt('Please enter a valid score')
-}
 
-if (score >= 90) {
-    alert('You have received an A')
-} else if (score >=80 && score<90) {
-    alert('You have received a B')
-} else if (score >=70 && score<80) {
-    alert('You have received a C')
-} else {
-    alert('You have failed')
-}
-console.log(score)
+
+
+
+// let score=prompt("score")
+// if (score > 100) {
+//     prompt('Please enter a valid score')
+// }
+
+// if (score >= 90) {
+//     alert('You have received an A')
+// } else if (score >=80 && score<90) {
+//     alert('You have received a B')
+// } else if (score >=70 && score<80) {
+//     alert('You have received a C')
+// } else {
+//     alert('You have failed')
+// }
+// console.log(score)
